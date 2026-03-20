082ec772b25a5ce5df80392d5a275c772c1d372d
Beltran, Nicolás <nicolasaiyajin0@gmail.com>
2026-03-19 21:10:01 -0300

    Add files via upload

---
+++ TP2.cs
@@ -0,0 +1,281 @@
+﻿// 11)
+int a = 25;
+int b = 30;
+
+if (a > b)
+{
+    Console.WriteLine("11)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("11)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 12)
+int c = 14;
+int d = 14;
+
+if (c <= d)
+{
+    Console.WriteLine("12)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("12)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 13)
+int e = 0;
+int f = 0;
+
+if (e == f)
+{
+    Console.WriteLine("13");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("13)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 14)
+int g = 10;
+int h = 10;
+
+if (g != h)
+{
+    Console.WriteLine("14)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("14)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 15)
+int i = 8;
+int j = 3;
+
+if (j >= i)
+{
+    Console.WriteLine("15)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("15)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+// 16)
+int k = 15;
+int l = 15;
+
+if (k < l)
+{
+    Console.WriteLine("16)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("16)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 17)
+int m = 100;
+int n = 50;
+
+if (m != n)
+{
+    Console.WriteLine("17)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("17)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 18)
+int o = 7;
+float p = 7.0f;
+
+if (o == p)
+{
+    Console.WriteLine("18)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("18)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 19)
+float q = 0.5f;
+int r = 0;
+
+if (q > r)
+{
+    Console.WriteLine("19)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("19)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 20)
+int s = -5;
+int t = -2;
+
+if (t < s)
+{
+    Console.WriteLine("20)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("20)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine();
+}
+// 21)
+if(5 > 3 && 10 < 20)
+{
+    Console.WriteLine("21)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("21)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 22)
+if(8 == 10 || 5 != 2)
+{
+    Console.WriteLine("22)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("22)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 23)
+if(!(10 == 10))
+{
+    Console.WriteLine("23)");
+    Console.WriteLine("Verdedero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("23)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 24)
+if((15 > 10 && !(5>10)))
+{
+    Console.WriteLine("24)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("24)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 25)
+if((10 + 2 > 10 || 5 * 2 < 8))
+{
+    Console.WriteLine("25)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("25)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+// 26)
+int temperatura = 35;
+bool humedad = false;
+if(temperatura > 30 && humedad == true)
+{
+    Console.WriteLine("26)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("26)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 27)
+int edad = 16;
+if (!(edad >= 18))
+{
+    Console.WriteLine("27)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("27)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 29)
+string nombre = "Ana";
+if (nombre == "Juan" || nombre == "Ana")
+{
+    Console.WriteLine("28)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("28)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
+// 30)
+if((5 * 2) + 10 > 15 && 20/4 == 5)
+{
+    Console.WriteLine("30)");
+    Console.WriteLine("Verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("30)");
+    Console.WriteLine("Falso");
+    Console.WriteLine("");
+}
