3b73ec7d778d302020d4b550f305fb5be6f903a5
Beltran, Nicolás <nicolasaiyajin0@gmail.com>
2026-05-05 12:41:12 -0300

    Update TP5.cs

--- TP5.cs
+++ TP5.cs
@@ -1,6 +1,6 @@
 ﻿// Ejercicio 1
 Console.WriteLine("Ejercicio 1");
-Console.WriteLine("")
+Console.WriteLine("");
 Console.WriteLine("ingrese una contraseña");
 string contraseña = "777";
 string contraseñaingresada = Console.ReadLine();
@@ -12,9 +12,9 @@ while (contraseñaingresada != contraseña)
 Console.WriteLine("contraseña correcta");
 
 // Ejercicio 2
-Console.WriteLine("")
+Console.WriteLine("");
 Console.WriteLine("Ejercicio 2");
-Console.WriteLine("")
+Console.WriteLine("");
 int contador = 5;
 while (contador >= 1)
 {
@@ -26,9 +26,9 @@ Console.WriteLine("¡Listo para despegar!");
 Console.WriteLine("¡Despegue!");
 
 // Ejercicio 3
-Console.WriteLine("")
+Console.WriteLine("");
 Console.WriteLine("Ejercicio 3");
-Console.WriteLine("")
+Console.WriteLine("");
 Console.WriteLine("Adivina el número secreto entre 1 y 10");
 int numerores = Convert.ToInt32(Console.ReadLine());
 while ( numerores != 7)
@@ -39,9 +39,9 @@ while ( numerores != 7)
 Console.WriteLine("¡Felicidades! Adivinaste el número.");
 
 // Ejercicio 4
-Console.WriteLine("")
+Console.WriteLine("");
 Console.WriteLine("Ejercicio 4:");
-Console.WriteLine("")
+Console.WriteLine("");
 int sumatoria = 0;
 Console.WriteLine("Ingrese números para sumar (0 para terminar):");
 int numero = Convert.ToInt32(Console.ReadLine());
